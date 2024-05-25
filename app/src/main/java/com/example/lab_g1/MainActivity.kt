6cc3d15d29ddf0ac873ac12a16cdea942c1b6b35
Room1zero1 <104586123+Room1zero1@users.noreply.github.com>
2024-05-25 19:35:37 +0700

    Ver 2 (Прописаны activity_main.xml и MainActivity.kt)

--- app/src/main/java/com/example/lab_g1/MainActivity.kt
+++ app/src/main/java/com/example/lab_g1/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.lab_g1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var inputEditText: EditText
+    private lateinit var outputTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        inputEditText = findViewById(R.id.inputEditText)
+        outputTextView = findViewById(R.id.outputTextView)
+    }
+
+    fun checkCharacter(character: Char) {
+        when (character) {
+            in 'a'..'z' -> {
+                if (character in "aeiouAEIOU") {
+                    outputTextView.text = "Это гласная буква"
+                } else {
+                    outputTextView.text = "Возможно, это согласная буква"
+                }
+            }
+            else -> {
+                outputTextView.text = "Введенный символ не является латинской строчной буквой"
+            }
+        }
+    }
+
+    fun onClickCheckButton(view: android.view.View) {
+        val inputText = inputEditText.text.toString()
+        if (inputText.isNotEmpty()) {
+            val character = inputText[0] // Берем первый символ
+            checkCharacter(character)
+        }
     }
 }
