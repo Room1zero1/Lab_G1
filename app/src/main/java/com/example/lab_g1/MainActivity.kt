9715a06cfd0510f3d2ec10fb15dae0d54fa49e53
Room1zero1 <104586123+Room1zero1@users.noreply.github.com>
2024-05-25 22:13:18 +0700

    Ver 4 (работа с функцией onClickCheckButton в MainActivity.kt) Приложение стало выводить корректные результаты

--- app/src/main/java/com/example/lab_g1/MainActivity.kt
+++ app/src/main/java/com/example/lab_g1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.lab_g1
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var inputEditText: EditText
     private lateinit var outputTextView: TextView
+    private lateinit var checkResultButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,30 +18,30 @@ class MainActivity : AppCompatActivity() {
 
         inputEditText = findViewById(R.id.inputEditText)
         outputTextView = findViewById(R.id.outputTextView)
+        checkResultButton = findViewById(R.id.checkResultButton)
+
+        checkResultButton.setOnClickListener {
+            val inputText = inputEditText.text.toString()
+            if (inputText.isNotEmpty()) {
+                val character = inputText[0]
+                val checkResult = checkCharacter(character)
+                outputTextView.text = checkResult // Отображение результата
+            }
+        }
     }
 
-    fun checkCharacter(character: Char) {
+    fun checkCharacter(character: Char): String { // Возвращает строку с результатом
         when (character) {
             in 'a'..'z' -> {
                 if (character in "aeiouAEIOU") {
-                    outputTextView.text = "Это гласная буква"
+                    return "Это гласная буква"
                 } else {
-                    outputTextView.text = "Возможно, это согласная буква"
+                    return "Возможно, это согласная буква"
                 }
             }
             else -> {
-                outputTextView.text = "Введенный символ не является латинской строчной буквой"
+                return "Введенный символ не является латинской строчной буквой"
             }
         }
     }
-
-    fun onClickCheckButton(view: android.view.View) {
-        val inputText = inputEditText.text.toString()
-        if (inputText.isNotEmpty()) {
-            val character = inputText[0]
-            val checkResult = checkCharacter(character) // Храним результат проверки
-            outputTextView.text = checkResult.toString() // Отображаем результат
-        }
-    }
-
 }
